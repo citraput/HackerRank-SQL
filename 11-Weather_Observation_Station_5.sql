90a94a93433695df9c7bef66a6aa9d5f9541ced6
citraput <49798775+citraput@users.noreply.github.com>
2021-03-30 09:20:07 +0700

    Update 11-Weather_Observation_Station_5.sql

--- 11-Weather_Observation_Station_5.sql
+++ 11-Weather_Observation_Station_5.sql
@@ -11,3 +11,16 @@ LIMIT 1;
 SELECT CITY, LENGTH(CITY) FROM STATION
 ORDER BY 2 DESC, 1
 LIMIT 1;
+
+                                                                                           /*
+Enter your query here.
+*/
+(SELECT CITY, LENGTH(CITY) as len_city FROM STATION
+GROUP BY 1
+ORDER BY len_city, CITY
+LIMIT 1)
+UNION ALL
+(SELECT CITY, LENGTH(CITY) as len_city FROM STATION
+GROUP BY 1
+ORDER BY len_city DESC, CITY
+LIMIT 1);
